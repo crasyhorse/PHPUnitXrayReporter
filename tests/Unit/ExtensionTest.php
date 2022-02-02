0604d76ecc96104eeaa4354083aa65e57f2989e1
Florian Weidinger <florianweidinger@bundeswehr.org>
2022-02-02 16:39:41 +0100

    Modified docblock of spec

--- tests/Unit/ExtensionTest.php
+++ tests/Unit/ExtensionTest.php
@@ -17,13 +17,32 @@ use CrasyHorse\Tests\Assets\Asset;
 class ExtensionTest extends TestCase
 {
     /**
+     * Cypress - cur.view führt zur CUR Übersichtsseite.
+     * 
+     * * Laden der Seite"cur.view"
+     * Ergebnis: Die Anwendung wechselt zur Komponente SZCurOverview
+     *
+     * @XRAY-testExecutionKey
+     * 
+     * @test
+     * @return void
+     */
+    public function spec_name(): void
+    {
+        $asset = new Asset();
+        $expected = 4;
+        $actual = $asset->add(2, 2);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
      * This is a test.
      *
      * @test
      *
      * @return void
      */
-    public function spec_name(): void
+    public function spec_name1(): void
     {
         $asset = new Asset();
         $expected = 4;
