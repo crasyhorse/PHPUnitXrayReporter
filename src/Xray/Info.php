499b29deb5f9b1aaa055b23e71baa3158b739dc8
Paul Friedemann <paulfriedemann@bundeswehr.org>
2022-02-03 14:17:25 +0100

    Filled class Info

---
+++ src/Xray/Info.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Crasyhorse\PhpunitXrayReporter\Xray;
+
+/**
+ * @author Paul Friedemann
+ */
+class Info
+{
+    /**
+     * @var string
+     */
+    private $projectKey;
+
+    /**
+     * @var string
+     */
+    private $summary;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $version;
+
+    /**
+     * @var string
+     */
+    private $revision;
+
+    /**
+     * @var string
+     */
+    private $user;
+
+    /**
+     * @var string
+     */
+    private $testPlanKey;
+
+    /**
+     * @var array
+     */
+    private $testEnvironments;
+
+    public function __construct(string $projectKey, string $summary, string $description, string $version, string $revision, string $user, string $testPlanKey, string $testEnvironments)
+    {
+        $this->projectKey = $projectKey;
+        $this->summary = $summary;
+        $this->description = $description;
+        $this->version = $version;
+        $this->revision = $revision;
+        $this->user = $user;
+        $this->testPlanKey = $testPlanKey;
+        $this->testEnvironments = $testEnvironments;
+    }
+
+    public function toJson(): string
+    {
+        // to fill
+    }
+}
