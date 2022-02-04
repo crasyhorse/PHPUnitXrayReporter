0934d9f8f870067ba20df37671b77c8f0e2d0662
Florian Weidinger <florianweidinger@bundeswehr.org>
2022-02-04 13:41:12 +0100

    Renamed ExtensionTest to ReporterTest

---
+++ tests/Unit/ReporterTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CrasyHorse\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Florian Weidinger
+ *
+ * @since 0.1.0
+ */
+class ReporterTest extends TestCase
+{
+}
