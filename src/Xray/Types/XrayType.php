1aad1f58d6b3bbae06c4859f2b212a3d3ddde004
Florian Weidinger <florianweidinger@bundeswehr.org>
2022-02-04 14:52:48 +0100

    Annotated interface

--- src/Xray/Types/XrayType.php
+++ src/Xray/Types/XrayType.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Crasyhorse\PhpunitXrayReporter\Xray\Types;
 
-interface XrayType {
-    
+/**
+ * Identifies a class that represents an XRay-JSON type.
+ *
+ * @author Florian Weidinger
+ *
+ * @since 0.1.0
+ */
+interface XrayType
+{
 }
