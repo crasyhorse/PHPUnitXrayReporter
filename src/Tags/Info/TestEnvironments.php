b5a135da6800734cf9941b3b670d935c9c1ff4cb
Florian Weidinger <florianweidinger@bundeswehr.org>
2022-02-05 13:52:31 +0100

    Override process method to strip off */

--- src/Tags/Info/TestEnvironments.php
+++ src/Tags/Info/TestEnvironments.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Crasyhorse\PhpunitXrayReporter\Tags\Info;
 
 use Crasyhorse\PhpunitXrayReporter\Tags\XrayTag;
+use Jasny\PhpdocParser\PhpdocException;
 use Jasny\PhpdocParser\Tag\ArrayTag;
 
 /**
@@ -19,5 +20,55 @@ class TestEnvironments extends ArrayTag implements XrayTag
     public function __construct()
     {
         parent::__construct('XRAY-INFO-testEnvironments', 'string');
+    }
+
+    /**
+     * Must be overriden because otherwise the closing PHPDoc comment
+     * consisting of an asterisk and a slash will show up in the list
+     * of test environments if this tag is the last one in the doc block.
+     *
+     * @param array<array-key, string> $notations
+     *
+     * @return array<array-key, string>
+     */
+    public function process(array $notations, string $value): array
+    {
+        if ($value === '') {
+            $notations[$this->name] = [];
+
+            return $notations;
+        }
+
+        $itemString = $this->stripOffClosingDocBlockComment(
+            $this->stripParentheses($value)
+        );
+
+        $items = $this->splitValue($itemString);
+
+        try {
+            $array = $this->toArray($items);
+        } catch (PhpdocException $exception) {
+            throw new PhpdocException(
+                "Failed to parse '@{$this->name} {$value}': ".$exception->getMessage(),
+                0,
+                $exception
+            );
+        }
+
+        $notations[$this->name] = $array;
+
+        return $notations;
+    }
+
+    /**
+     * Removes the closing PHPDoc comment consisting of an asterisk and a slash.
+     *
+     * @return string|string[]|null
+     */
+    protected function stripOffClosingDocBlockComment(string $value)
+    {
+        preg_match('/([^\*\/ ]*)(?!< \*\/$)/', $value, $matches);
+
+        return $matches[0];
     }
 }
