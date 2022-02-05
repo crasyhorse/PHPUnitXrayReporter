115ecc896d1979e5711f887c60d27925199fcb13
Florian Weidinger <florianweidinger@bundeswehr.org>
2022-02-05 14:58:37 +0100

    Started implementing Reporter class

--- src/Reporter/Reporter.php
+++ src/Reporter/Reporter.php
@@ -4,24 +4,51 @@ declare(strict_types=1);
 
 namespace Crasyhorse\PhpunitXrayReporter\Reporter;
 
+use Crasyhorse\PhpunitXrayReporter\Parser\Parser;
+use Crasyhorse\PhpunitXrayReporter\Reporter\Results\TestResult;
+use Crasyhorse\PhpunitXrayReporter\Tags\XrayTag;
+
 /**
  * Processes test results and the meta information for the annotations.
- * 
+ *
  * @author Florian Weidinger
- * 
+ *
  * @since 0.1.0
  */
-class Reporter {
+class Reporter
+{
+    /**
+     * @var Parser
+     */
+    private $parser;
 
     /**
-     * @param string $test Test result from PHP-Unit
-     * @param string $message Error message if $test is FAIL, ERROR, WARNING or RISKY
-     * @param float $time Duration of the test
-     * @param array<int, string> $meta Information evaluated from the annotations
+     * @var array<array-key, TestResult>
      */
-    public function __construct(string $test, string $message = null, float $time, array $meta)
+    private $testResults;
+
+    /**
+     * @param array<array-key, string>  $whitelistedTags      Allow additional tags like @test or @dataProvider
+     * @param array<array-key, string>  $blacklistedTags      Remove tags like @param or @return
+     * @param array<array-key, XrayTag> $additionalCustomTags Additional Tags of type XrayTag
+     */
+    public function __construct(array $whitelistedTags = [], array $blacklistedTags = [], array $additionalCustomTags = [])
     {
-        # code...
+        $this->parser = new Parser($whitelistedTags, $blacklistedTags, $additionalCustomTags);
+        $this->testResults = [];
     }
 
+    public function add(TestResult $testResult): void
+    {
+        $this->testResults[] = $testResult;
+    }
+
+    public function processResults(): void
+    {
+        foreach ($this->testResults as $result) {
+            $parsed = $this->parser->parse($result);
+
+            var_dump($parsed);
+        }
+    }
 }
