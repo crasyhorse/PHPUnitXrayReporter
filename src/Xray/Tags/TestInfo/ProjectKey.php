159aefacc994205c7828ef4c1443118e3e206757
Paul Friedemann <paulfriedemann@bundeswehr.org>
2022-02-10 18:00:17 +0100

    Implemented ModifiedWordTag so that wordtags are cutted after "," This ist usefull because we dont want comma separated strings as words

--- src/Xray/Tags/TestInfo/ProjectKey.php
+++ src/Xray/Tags/TestInfo/ProjectKey.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Crasyhorse\PhpunitXrayReporter\Xray\Tags\TestInfo;
 
 use Crasyhorse\PhpunitXrayReporter\Xray\Tags\XrayTag;
-use Jasny\PhpdocParser\Tag\WordTag;
+use Crasyhorse\PhpunitXrayReporter\XRAY\Tags\ModifiedWordTag;
 
 /**
  * Represents the XrayTag that correlats with the XrayType ProjectKey.
@@ -14,7 +14,7 @@ use Jasny\PhpdocParser\Tag\WordTag;
  *
  * @since 0.1.0
  */
-class ProjectKey extends WordTag implements XrayTag
+class ProjectKey extends ModifiedWordTag implements XrayTag
 {
     public function __construct()
     {
