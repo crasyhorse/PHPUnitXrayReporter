0a70d5ecdc8ab75f5fbfb5a28ac8c533dddeb973
Paul Friedemann <paulfriedemann@bundeswehr.org>
2022-02-16 18:00:30 +0100

    WIP: bring ParserTest forward

--- tests/Assets/PseudoSpec.php
+++ tests/Assets/PseudoSpec.php
@@ -37,7 +37,7 @@ class PseudoSpec
     }
 
     /**
-     * Update test execution DEMO-667 including the Info object.
+     * Update test execution DEMO-667 with little information.
      *
      * @XRAY-testExecutionKey DEMO-667
      * @XRAY-TESTS-testKey DEMO-123
@@ -48,10 +48,15 @@ class PseudoSpec
     }
 
     /**
-     * Update test execution DEMO-668 including the Info object.
+     * Successful test result with Test and TestInfo objects.
      *
+     * @test
      * @XRAY-testExecutionKey DEMO-668
+     *
      * @XRAY-TESTS-testKey DEMO-123
+     * @XRAY-TESTS-comment This Test should return PASS
+     * @XRAY-TESTS-defects DEMO-1,DEMO-2
+     *
      * @XRAY-TESTINFO-projectKey DEMO
      * @XRAY-TESTINFO-testType Generic
      * @XRAY-TESTINFO-requirementKeys DEMO-1,DEMO-2,DEMO-3
@@ -64,20 +69,28 @@ class PseudoSpec
     }
 
     /**
-     * Update test execution DEMO-669 including the Info object.
+     * Update test execution DEMO-669.
+     * This is really cool!
      *
      * @XRAY-testExecutionKey DEMO-669
-     * @XRAY-INFO-project DEMO
-     * @XRAY-INFO-summary Update test execution DEMO-669 including the Info object
-     * @XRAY-INFO-description This is some kind of a test double for testing the Parser class. That means it is only a pseudo spec.
-     * @XRAY-INFO-version 1.0
-     * @XRAY-INFO-revision 1.0.1
-     * @XRAY-INFO-user CalamityCoyote
-     * @XRAY-INFO-testEnvironments PHP-Unit
+     *
      * @XRAY-TESTS-testKey DEMO-124
+     * @XRAY-TESTS-comment This Test should return FAIL
+     * @XRAY-TESTS-defects DEMO-1,DEMO-2
+     *
+     * @XRAY-TESTINFO-projectKey DEMO
+     * @XRAY-TESTINFO-testType Generic
+     * @XRAY-TESTINFO-requirementKeys DEMO-1,DEMO-2
+     * @XRAY-TESTINFO-labels workInProgress,Bug,NeedsTriage
+     * @XRAY-TESTINFO-definition The Test does nothing
      */
     public function spec4(): void
     {
         // code...
+    }
+
+    public function spec5(): void
+    {
+        // code
     }
 }
